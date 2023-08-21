3b0bc82e0c924ad5966c59522be0bd70df1451d9
prodhanm <mustafizur.prodhan@gmail.com>
2023-08-21 16:19:18 -0400

    added more unit test to check for any issues with the program.

--- test.py
+++ test.py
@@ -8,5 +8,18 @@ class TestWordPrefix(unittest.TestCase):
         result = prefix in word
         self.assertTrue(result)
 
+    def test_prefix2(self):
+        prefix = "pro"
+        word = "primer"
+        result = prefix in word
+        self.assertFalse(result)
+
+    def test_prefix3(self):
+        prefix = "pro"
+        word = "provocation"
+        result = prefix in word
+        self.assertTrue(result)
+
+
 if __name__ == "__main___":
     unittest.main()
